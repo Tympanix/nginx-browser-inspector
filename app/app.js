f9f84d9f5d4a0344f832fd33def76642d84b7419
Mathias <tympanix@gmail.com>
2017-01-05 19:05:29 +0100

    Toggle button angular integration

--- app/app.js
+++ app/app.js
@@ -2,4 +2,16 @@ var app = angular.module("nginxTrustedBrowsers", []);
 
 app.controller('mainController', ['$scope', function($scope){
     $scope.hello = "Hello there! Sup!"
+
+    $scope.list = function(dic) {
+        let keys = Object.keys(dic)
+        let filtered = keys.filter(function(value){
+            return dic[value] === true
+        })
+        return filtered.join(', ')
+    }
+
+    $scope.os = {}
+    $scope.browsers = {}
+    $scope.devices = {}
 }]);
--- app/slider.js
+++ app/slider.js
@@ -1,20 +1,3 @@
-var app = angular.module("nginxTrustedBrowsers", []);
-
-app.controller('mainController', ['$scope', function($scope){
-    $scope.hello = "Hello there! Sup!"
-
-    $scope.list = function(dic) {
-        let keys = Object.keys(dic)
-        let filtered = keys.filter(function(value){
-            return dic[value] === true
-        })
-        return filtered.join(', ')
-    }
-
-    $scope.os = {}
-    $scope.browsers = {}
-    $scope.devices = {}
-}]);
 'use strict'
 
 angular.module('nginxTrustedBrowsers').directive('toggle', function() {
